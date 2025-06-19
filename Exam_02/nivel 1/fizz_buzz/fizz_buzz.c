0ff90af372cc518511853e83b538dcbb5946c47c
IRENE DIAZ CARMONA <82152635+irene-diaz@users.noreply.github.com>
2025-06-19 21:19:30 +0200

    Add files via upload

---
+++ Exam_02/nivel 1/fizz_buzz/fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+void ft_putnbr(int n)
+{
+    int numbBase;
+    if(n > 10)
+        ft_putnbr(n / 10);
+    numbBase = (n % 10) + '0';
+    write (1, &numbBase, 1);
+}
+
+int main(void)
+{
+ int i=0;
+ while(i<=100)
+ {
+    if(i%3==0)
+    {
+        write(1,"fizz", 4);
+    }
+    if(i%5==0)
+    {
+        write(1,"buzz", 4);
+    }
+    if(i % 3 != 0 && i % 5 != 0)
+        ft_putnbr(i);
+   write(1, "\n", 1);
+   i++;
+ }
+
+}
