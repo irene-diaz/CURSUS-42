6f9ebc7b14f63a938e40e1d63bc4e2ddbfc8d98c
IRENE DIAZ CARMONA <82152635+irene-diaz@users.noreply.github.com>
2025-07-22 16:26:16 +0200

    Update sort_list.c

--- Exam_02/nivel 4/sort_list/sort_list.c
+++ Exam_02/nivel 4/sort_list/sort_list.c
@@ -1,5 +1,4 @@
 #include "list.h"
-#include <unistd.h>
 /* 
     - lst: es el puntero al primer nodo de la lista enlazada.
 
@@ -8,33 +7,34 @@
     distinto de 0 si están bien ordenados.
 */
 
-t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
+t_list *sort_list(t_list *lst, int (*cmp)(int, int))
 {
-	int tmp;            // Variable temporal para hacer el intercambio de datos
-	t_list *head;
+    int tmp;                // Variable temporal para hacer el intercambio de datos
+    t_list *head;
 
-	head = lst;         // Guardamos el puntero al inicio de la lista
+    head = lst;             // Guardamos el puntero al inicio de la lista
 
-	// Recorremos la lista mientras existan al menos dos nodos para comparar
-	while (lst != NULL && lst->next != NULL)
-	{
-		// Usamos la función de comparación para ver si los elementos están mal ordenados
-		if ((*cmp)(lst->data, lst->next->data) == 0)
-		{
-			// Si están mal ordenados, intercambiamos los datos de los nodos
-			tmp = lst->data;
-			lst->data = lst->next->data;
-			lst->next->data = tmp;
+    // Recorremos la lista mientras existan al menos dos nodos para comparar
+    while (lst != NULL && lst->next != NULL)
+    {
+        // Usamos la función de comparación para ver si los elementos están mal ordenados
+        if ((*cmp)(lst->data, lst->next->data) > 0)  // Cambié == 0 por > 0
+        {
+            // Si están mal ordenados, intercambiamos los datos de los nodos
+            tmp = lst->data;
+            lst->data = lst->next->data;
+            lst->next->data = tmp;
 
-			// Después de un intercambio, volvemos al inicio de la lista
-			lst = head;
-		}
-		else
-			// Si están bien ordenados, seguimos al siguiente par
-			lst = lst->next;
-	}
-	// Devolvemos el puntero al inicio de la lista ya ordenada
-	return (head);
+            // Después de un intercambio, volvemos al inicio de la lista
+            lst = head;   // Reiniciamos lst para comparar todos los nodos desde el inicio
+        }
+        else
+            // Si están bien ordenados, seguimos al siguiente par
+            lst = lst->next;
+    }
+
+    // Devolvemos el puntero al inicio de la lista ya ordenada
+    return (head);
 }
 
 /*int ascending(int a, int b)
