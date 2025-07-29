658a131fe3c9aa4814a3706774ee085f6674ac32
IRENE DIAZ CARMONA <82152635+irene-diaz@users.noreply.github.com>
2025-07-29 07:54:00 +0200

    Update sort_int_tab.c

--- Exam_02/nivel 4/sort_int_tab/sort_int_tab.c
+++ Exam_02/nivel 4/sort_int_tab/sort_int_tab.c
@@ -1,63 +1,52 @@
 //#include <stdio.h>
 
-typedef struct s_list t_list;
-
-struct s_list
+void	sort_int_tab(int *tab, unsigned int size)
 {
-	int     data;
-	t_list  *next;
-};
+	int	temp;
+	int	swapped;
+	int	i;
 
-t_list *sort_list(t_list *lst, int (*cmp)(int, int))
-{
-    int temp;
-    int swapped;
-    t_list *head = lst;  // Guardamos la referencia al primer nodo de la lista
-
-    // Verificamos si la lista está vacía o tiene un solo elemento
-    if (lst == 0 || lst->next == 0)
-        return lst;
-
-    do {
-        swapped = 0;
-        // Recorremos la lista comparando los nodos adyacentes
-        while (lst != 0 && lst->next != 0) {
-            if ((*cmp)(lst->data, lst->next->data) > 0) {
-                // Intercambiamos los datos de los nodos
-                temp = lst->data;
-                lst->data = lst->next->data;
-                lst->next->data = temp;
-
-                swapped = 1;  // Indicamos que hubo un intercambio
-            }
-            lst = lst->next;  // Avanzamos al siguiente nodo
-        }
-        // Reiniciamos el puntero lst al principio de la lista
-        lst = head;
-
-    } while (swapped);  // Repetimos el proceso si hubo intercambios
-
-    return head;  // Retornamos el inicio de la lista
+	// si hay menos de dos numeros, ya estan ordenados
+	if (size < 2)
+		return ;
+	do
+	{
+		swapped = 0;
+		i = 0;
+		// Recorre el array hasta el penúltimo elemento
+		while (i < size - 1)
+		{
+			// Si el elemento actual es mayor que el siguiente, los intercambia
+			if (tab[i] > tab[i + 1])
+			{
+				temp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = temp;
+				swapped = 1;
+			}
+			i++;
+		}
+	} while (swapped);
 }
 
 /*unsigned int ft_strlen(int *str)
 {
-        int i=0;
-        while(*str)
-        {
-            str++;
-            i++;
-        }
-        return i;
+		int i=0;
+		while(*str)
+		{
+			str++;
+			i++;
+		}
+		return (i);
 }
-int main(void)
+int	main(void)
 {
-    int arr[] = {5, 2, 8, 1, 3};
-    unsigned int size = ft_strlen(arr);
+	int arr[] = {5, 2, 8, 1, 3};
+	unsigned int size = ft_strlen(arr);
 
-    sort_int_tab(arr, size);
+	sort_int_tab(arr, size);
 
-    for (unsigned int i = 0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+	for (unsigned int i = 0; i < size; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
 }*/
