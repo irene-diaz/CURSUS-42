2cb184185cb9ab1523f1927eba13b3386fca423f
IRENE DIAZ CARMONA <82152635+irene-diaz@users.noreply.github.com>
2026-03-06 10:14:45 +0100

    Actualizar init.c(incluir mis propias funciones de ft_atol y ft_atoi)

--- philo/src/init.c
+++ philo/src/init.c
@@ -20,13 +20,13 @@ int	init_data(t_data *data, int argc, char **argv)
 	if (argc != 5 && argc != 6)
 		return (printf("Invalid number of arguments\n"), 1);
 	if (argc == 6)
-		data->must_eat_count = atoi(argv[5]);
+		data->must_eat_count = ft_atoi(argv[5]);
 	else
 		data->must_eat_count = -1;
-	data->nb_philo = atoi(argv[1]);
-	data->time_to_die = atol(argv[2]);
-	data->time_to_eat = atol(argv[3]);
-	data->time_to_sleep = atol(argv[4]);
+	data->nb_philo = ft_atoi(argv[1]);
+	data->time_to_die = ft_atol(argv[2]);
+	data->time_to_eat = ft_atol(argv[3]);
+	data->time_to_sleep = ft_atol(argv[4]);
 	if (data->nb_philo <= 0 || data->time_to_die <= 0 || data->time_to_eat <= 0
 		|| data->time_to_sleep <= 0)
 		return (printf("Invalid arguments\n"), 1);
