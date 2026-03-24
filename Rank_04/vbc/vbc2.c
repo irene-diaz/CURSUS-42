75b728490152d92b57cb36844f30f95b6b1eaa98
Irene Diaz Carmona <idiaz-ca@c2r11s1.42madrid.com>
2026-03-24 18:13:25 +0100

    vbc2 practica

--- Rank_04/vbc/vbc2.c
+++ Rank_04/vbc/vbc2.c
@@ -1,3 +1,27 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+char *s;
+
+void unexpected(char c)
+{
+    if(c)
+        printf("Unexpected token '%c'\n", c);
+    else
+        printf("Unexpected end of input\n");
+}
+
+int sum(void);
+
+int factor(void)
+{
+    int n;
+
+    if(*s == '\0')
+        unexpected(0);
+    
+    if(isdigit(*s))
+       return *s++ - '0';
+
+}
