e0b2e8a471dda11dbf316de58b250e0b3b9f958d
iredica <iredica@gmail.com>
2026-03-25 18:15:32 +0100

    actualizacion

--- Rank_04/ft_popen/ft_popen2.c
+++ Rank_04/ft_popen/ft_popen2.c
@@ -1,54 +1,55 @@
 #include <fcntl.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int ft_popen(const char *file, const char *av[], char type)
+int	ft_popen(const char *file, char *const argv[], char type)
 {
-    if(!file || !av || (type != 'r' && type !='w'))
-        return -1;
+	if(!file || !argv || (type != 'r' && type != 'w'))
+		return -1;
 
-    int fd[2];
+	int fd[2];
 
-    if(pipe(fd) < 0)
-        return -1;
+	if(pipe(fd) < 0)
+		return -1;
 
-    pid_t pid = fork();
+	pid_t pid = fork();
 
-    if(pid < 0)
-    {
-        close(fd[0]);
-        close(fd[1]);
-        return -1;
-    }
+	if(pid < 0)
+	{
+		close(fd[0]);
+		close(fd[1]);
+		return -1;
+	}
 
-    if(pid == 0)
-    {
-        if(type == 'r')
-        {
-            close(fd[0]);
-            if(dup2(fd[1], STDOUT_FILENO) < 0)
-                exit(1);
-            close(fd[1]);
-        }
-        else{
-            close(fd[1]);
-            if(dup2(fd[0], STDIN_FILENO) < 0)
-                exit(1);
-            close(fd[0]);
-        }
+	if(pid==0)
+	{
+		if(type== 'r')
+		{
+			close(fd[0]);
+			if(dup2(fd[1], STDOUT_FILENO) < 0)
+				exit(1);
+			close(fd[1]);
+		}
+		else
+		{
+			close(fd[1]);
+			if(dup2(fd[0], STDIN_FILENO) < 0)
+				exit(1);
+			close(fd[0]);
+		}
 
-        execvp(file, av);
-        exit(1);
-    }
+		execvp(file, argv);
+		exit(1);
+	}
 
-    if(type=='r')
-    {
-        close(fd[1]);
-        return fd[0];
-    }
-    else
-    {
-        close(fd[0]);
-        return fd[1];
-    }
+	if(type == 'r')
+	{
+		close (fd[1]);
+		return fd[0];
+	}
+	else
+	{
+		close (fd[0]);
+		return fd[1];
+	}
 }
--- Rank_04/vbc/vbc2.c
+++ Rank_04/vbc/vbc2.c
@@ -10,6 +10,8 @@ void unexpected(char c)
         printf("Unexpected token '%c'\n", c);
     else
         printf("Unexpected end of input\n");
+
+    exit(1);
 }
 
 int sum(void);
@@ -20,8 +22,68 @@ int factor(void)
 
     if(*s == '\0')
         unexpected(0);
-    
-    if(isdigit(*s))
-       return *s++ - '0';
 
+    if (isdigit(*s))
+        return *s++ - '0';
+
+    if(*s == '(')
+    {
+        s++;
+        n = sum();
+
+        if(*s != ')')
+            unexpected(*s);
+        
+        s++;
+        return n;
+    }
+
+    unexpected(*s);
+    return 0;
+}
+
+int product(void)
+{
+    int n;
+    n=factor();
+
+    while(*s == '*')
+    {
+        s++;
+        n *= factor();
+    }
+
+    return n;
+}
+
+int sum(void)
+{
+    int n;
+    n = product();
+
+    while(*s == '+')
+    {
+        s++;
+        n += product();
+    }
+
+    return n;
+}
+
+int main(int argc, char *argv[])
+{
+    int result;
+
+    if(argc != 2)
+        return 1;
+
+    s = argv[1];
+
+    result= sum();
+
+    if (*s != '\0')
+        unexpected(*s);
+
+    printf("%d\n", result);
+    return 0;
 }
