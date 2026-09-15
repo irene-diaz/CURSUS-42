4885c0105ed91ed3435a97a0c70276df49831b34
iredica <iredica@gmail.com>
2026-09-15 13:22:03 +0200

    Avance

--- Rank_06/mini_serv/mini_serv2.c
+++ Rank_06/mini_serv/mini_serv2.c
@@ -124,3 +124,83 @@ void	remove_client(t_client **clients, t_client *client)
 		tmp = tmp->next;
 	}
 }
+
+int	main(int argc, char **argv)
+{
+	int					server_fd;
+	int					client_fd;
+	int					max_fd;
+	int					i;
+	int					ret;
+	int					next_id;
+	char				buf[60000];
+	char				msg[70000];
+	char				*newline;
+	t_client			*clients;
+	t_client			*new_client;
+	t_client			*next;
+	t_client			*tmp;
+	fd_set				readfds;
+	fd_set				writefds;
+	struct sockaddr_in	addr;
+	int					j;
+
+	if (argc != 2)
+	{
+		write(2, "Wrong number of arguments\n", 26);
+		return (1);
+	}
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd < 0)
+		fatal();
+	// assign IP, PORT
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htonl(2130706433); // 127.0.0.1
+	addr.sin_port = htons(atoi(argv[1]));
+	// Binding newly created socket to given IP and verification
+	if ((bind(server_fd, (const struct sockaddr *)&addr, sizeof(addr))) < 0)
+		fatal();
+	if (listen(server_fd, 128) < 0)
+		fatal();
+	clients = NULL;
+	next_id = 0;
+	while (1)
+	{
+		FD_ZERO(&readfds);
+		FD_ZERO(&writefds);
+		FD_SET(server_fd, &readfds);
+		tmp = clients;
+		max_fd = server_fd;
+		while (tmp)
+		{
+			FD_SET(tmp->fd, &readfds);
+			if (tmp->out_len > 0)
+				FD_SET(tmp->fd, &writefds);
+			if (tmp->fd > max_fd)
+				max_fd = tmp->fd;
+			tmp = tmp->next;
+		}
+		ret = select(max_fd + 1, &readfds, &writefds, NULL, NULL);
+		if (ret < 0)
+			fatal();
+		if (FD_ISSET(server_fd, &readfds))
+		{
+			client_fd = accept(server_fd, NULL, NULL);
+			if (client_fd < 0)
+				fatal();
+			new_client = malloc(sizeof(t_client));
+			if (!new_client)
+				fatal();
+			new_client->fd = client_fd;
+			new_client->id = next_id++;
+			new_client->in = NULL;
+			new_client->in_len = 0;
+			new_client->out = NULL;
+			new_client->out_len = 0;
+			new_client->next = clients;
+			clients = new_client;
+			sprintf(msg, "server: client %d just arrived\n", new_client->id);
+			broadcast(clients, new_client->fd, msg, strlen(msg));
+		}
+	}
+}
