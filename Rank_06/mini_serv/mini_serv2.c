f75086e46b18df3e33acc94c3719750629a7a0c7
iredica <iredica@gmail.com>
2026-09-15 08:55:36 +0200

    prueba del Rank_06

--- Rank_06/mini_serv/mini_serv2.c
+++ Rank_06/mini_serv/mini_serv2.c
@@ -45,8 +45,8 @@ void	add_to_buffer(char **buf, int *len, char *data, int data_len)
 
 void	remove_from_buffer(char **buf, int *len, int amount)
 {
-	char *newbuf;
-	int i;
+	char	*newbuf;
+	int		i;
 
 	if (amount >= *len)
 	{
@@ -55,20 +55,72 @@ void	remove_from_buffer(char **buf, int *len, int amount)
 		*len = 0;
 		return ;
 	}
-
 	newbuf = malloc(*len - amount + 1);
 	if (!newbuf)
 		fatal();
-
 	i = 0;
 	while (i < *len - amount)
 	{
 		newbuf[i] = (*buf)[amount + i];
 		i++;
 	}
-
 	newbuf[*len - amount] = '\0';
 	free(*buf);
 	*buf = newbuf;
 	*len -= amount;
+}
+
+void	queue_message(t_client *client, char *msg, int len)
+{
+	add_to_buffer(&client->out, &client->out_len, msg, len);
+}
+
+void	broadcast(t_client *clients, int except_fd, char *msg, int len)
+{
+	t_client	*tmp;
+
+	tmp = clients;
+	while (tmp)
+	{
+		if (tmp->fd != except_fd)
+			queue_message(tmp, msg, len);
+		tmp = tmp->next;
+	}
+}
+
+void	send_pending(t_client *clients)
+{
+	int	ret;
+
+	if (!clients->out || clients->out_len == 0)
+		return ;
+	ret = send(clients->fd, clients->out, clients->out_len, 0);
+	if (ret > 0)
+		remove_from_buffer(&clients->out, &clients->out_len, ret);
+}
+
+void	remove_client(t_client **clients, t_client *client)
+{
+	t_client	*tmp;
+	t_client	*prev;
+
+	tmp = *clients;
+	prev = NULL;
+	while (tmp)
+	{
+		if (tmp == client)
+		{
+			if (prev)
+				prev->next = tmp->next;
+			else
+				*clients = tmp->next;
+			close(tmp->fd);
+			free(tmp->in);
+			free(tmp->out);
+			free(tmp);
+			return ;
+		}
+		prev = tmp;
+		tmp = tmp->next;
+	}
 }
